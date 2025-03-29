880a86f0d9c1e8c93af364d87c72e6d31142fa9e
Zaahid Vohra <67497624+zaahidvohra@users.noreply.github.com>
2025-03-29 16:16:34 +0530

    Frontend Structure
    I have only created the react app in frontend folder and added bootstrap and created a basic component for bootstrap testing.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Try from "./components/try";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <Try />
+    </div>
+  );
+}
+
+export default App;
