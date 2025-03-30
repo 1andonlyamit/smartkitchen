139c1cf48f9744c88a2cadc3d0c5ae9f804b5552
amit suthar <amit.suthar@consectus.com>
2025-03-30 23:05:09 +0530

    ROUTE ADDED: inventory route

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ const dbService = new DatabaseService();
 app.post('/login', (req, res) => userController.login(req, res));
 app.post('/register', (req, res) => userController.register(req, res));
 
+const inventoryRoutes = require('./router/inventory_r');
+app.use('/inventory', inventoryRoutes);
+
 app.use("*", function (req, res) {
     res.status(200).send({ "message": "Access Denied" })
 })
