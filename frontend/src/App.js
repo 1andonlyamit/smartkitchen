5a7a732719b390495c28bfdb1d3a0ad7de9f7ff6
Zaahid Vohra <67497624+zaahidvohra@users.noreply.github.com>
2025-03-30 08:50:49 +0530

    pages folder updated
    Created all pages and added its routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,11 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from "./components/login-signup/Login";
 import Signup from "./components/login-signup/Signup";
 import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Recepies from "./pages/Recepies";
+import Inventory from "./pages/Inventory";
+import Forecasting from "./pages/Forecasting";
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
@@ -14,6 +19,11 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/recepies" element={<Recepies/>}/>
+        <Routes path="/inventory" element={<Inventory/>}/>
+        <Routes path="/forecasting" element={<Forecasting/>}/>
+        <Routes path="/analytics" element={<Analytics/>}/>
       </Routes>
       </div>
   );
